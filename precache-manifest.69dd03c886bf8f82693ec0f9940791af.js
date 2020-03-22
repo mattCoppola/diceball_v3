a57efc72a66d2bbc8aa3c2520617d3d24c27c964
Matt Coppola <matt.coppola@gmail.com>
2020-03-22 16:22:13 -0500

    Updates

---
+++ precache-manifest.69dd03c886bf8f82693ec0f9940791af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef87752fede05525bec708e4408f4d7",
+    "url": "/diceball_v3/index.html"
+  },
+  {
+    "revision": "7b23c029f86959759613",
+    "url": "/diceball_v3/static/css/main.fa9425e3.chunk.css"
+  },
+  {
+    "revision": "bb02560b93d174d6cc14",
+    "url": "/diceball_v3/static/js/2.1e3e9fd3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/diceball_v3/static/js/2.1e3e9fd3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b23c029f86959759613",
+    "url": "/diceball_v3/static/js/main.6d3505fe.chunk.js"
+  },
+  {
+    "revision": "95c9d8d42792889ce5a4",
+    "url": "/diceball_v3/static/js/runtime-main.b645bc3d.js"
+  }
+]);
